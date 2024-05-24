ad8f6450c979aa89d29aa28c2e9e78f6b1458ca5
Electrix67130 <julien.schubnel@outlook.fr>
2024-05-24 14:55:15 +0200

    Correctif concernant la version de l'API

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/user', userRoutes);
+app.use('/use', userRoutes);
 
 app.get('/', (req, res) => {
     res.send(`Bienvenue sur mon API Breizhsport Utilisateur Ugur !`);
